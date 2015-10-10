b7e725a0a5f370e43e8f51eeb46d0e9a76385f9b
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2015-10-10 22:00:46 +0200

    Fix interface for sniff and httpdump

--- zprezto/.zprezto-zack/aliases.sh
+++ zprezto/.zprezto-zack/aliases.sh
@@ -47,8 +47,8 @@ alias localip="ipconfig getifaddr en0"
 alias ips="ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[0-9]\+\)\|[a-fA-F0-9:]\+\)' | awk '{ sub(/inet6? (addr:)? ?/, \"\"); print }'"
 
 # View HTTP traffic
-alias sniff="sudo ngrep -d 'en1' -t '^(GET|POST) ' 'tcp and port 80'"
-alias httpdump="sudo tcpdump -i en1 -n -s 0 -w - | grep -a -o -E \"Host\: .*|GET \/.*\""
+alias sniff="sudo ngrep -d 'en0' -t '^(GET|POST) ' 'tcp and port 80'"
+alias httpdump="sudo tcpdump -i en0 -n -s 0 -w - | grep -a -o -E \"Host\: .*|GET \/.*\""
 
 # Make
 alias mk='make -f makefile.mk'
