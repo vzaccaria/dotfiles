f40f1dca793961759914fcdf18d5cbb45110128a
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2016-04-18 11:07:51 +0200

    feat: add private layer

--- spacemacs/.emacs.vz/agenda.el
+++ spacemacs/.emacs.vz/agenda.el
@@ -1,10 +1,6 @@
 (setq org-agenda-custom-commands '(("w" "My Agenda" ((agenda "") (todo "")))))
-
 (setq org-columns-default-format "%40ITEM(Task) %17Effort(Estimated Effort){:} %CLOCKSUM")
-
-
 (setq org-agenda-todo-list-sublevels nil)
-
 (setq org-agenda-files
       (list "~/Dropbox/org"
             "~/Dropbox/org/howtos"
