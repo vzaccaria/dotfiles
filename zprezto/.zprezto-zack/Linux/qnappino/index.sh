34816eae04deecd08ceacd89a1f0f23ce2fea321
administrator <vittorio.zaccaria@gmail.com>
2016-07-22 12:04:21 +0200

    feat: add spacemacs docker runner

--- zprezto/.zprezto-zack/Linux/qnappino/index.sh
+++ zprezto/.zprezto-zack/Linux/qnappino/index.sh
@@ -7,3 +7,15 @@ export EDITOR=emacsclient
 export GIT_EDITOR=emacsclient
 export SHELL=/opt/bin/zsh
 alias vi=vim
+
+spacemacs() {
+	docker run -ti --rm \
+	-v /etc/localtime:/etc/localtime:ro                     \
+	-v /share/CACHEDEV1_DATA/homes/admin/projects:/projects \
+	-v /share/CACHEDEV1_DATA/homes/admin/projects/dot-stack:/root/.stack \
+	-v /share/CACHEDEV1_DATA/homes/admin/projects/dot-local:/root/.local \
+	-e DISPLAY=$DISPLAY \
+	-e PATH=/root/.local/bin:/opt/bin:/opt/sbin:/bin:/sbin:/usr/bin:/usr/sbin:/usr/bin/X11:/usr/local/sbin:/usr/local/bin \
+	--name spacemacs \
+	vzaccaria/devenv:latest 
+}
