bf8c0e6ca2fac2a27211f0cd568471c645ff443b
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2017-02-22 16:29:50 +0100

    minor: some minor work

--- osx-spacemacs/.emacs.vz/private/personal/config.el
+++ osx-spacemacs/.emacs.vz/private/personal/config.el
@@ -1,11 +1,3 @@
-(setenv "PATH" (format "%s:%s" "/usr/local/bin" (getenv "PATH")))
-(setenv "PATH" (format "%s:%s" "/usr/local/texlive/2013/bin/universal-darwin" (getenv "PATH")))
-(setenv "PATH" (format "%s:%s" "/Applications/Mathematica.app/Contents/MacOS" (getenv "PATH")))
-(setenv "PATH" (format "%s:%s" "/Users/zaccaria/.local/bin" (getenv "PATH")))
-(setenv "PATH" (format "%s:%s" "/root/.local/bin" (getenv "PATH")))
-(setenv "PATH" (format "%s:%s" "/Users/zaccaria/node_modules/.bin" (getenv "PATH")))
-(setenv "NODE_PATH" (format "%s:%s" "/usr/local/lib/node_modules" (getenv "NODE_PATH")))
-
 (setenv "DOCKER_TLS_VERIFY" "1")
 (setenv "DOCKER_HOST" "tcp://192.168.1.120:2376")
 (setenv "DOCKER_CERT_PATH" "/Users/zaccaria/dotfiles/zprezto/.zprezto-zack/Darwin/qnap-certs")
--- setup/readme.md
+++ setup/readme.md
@@ -1,16 +1,17 @@
-Fonts to be installed:
-======================
+# Orgmode capture
+
+See here: https://github.com/aaronbieber/org-protocol-handler
+
+# Fonts to be installed:
 
 -   https://github.com/powerline/fonts.git
 
-Paths to be added
-=================
+# Paths to be added
 
 -   Brew installed Python uses the following directory for global
     installations:
 
 <!-- -->
-
     ~/Library/Python/2.7/bin
 
 These should be in the PATH.
