df14190764cc875b06f6b09f4c58d593c6ba1bb9
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2018-03-04 21:37:21 +0100

    fix: haskell repl (private.xml, packages.el, .spacemacs)

--- spacemacs/.emacs.vz/private/haskell-config/packages.el
+++ spacemacs/.emacs.vz/private/haskell-config/packages.el
@@ -1,6 +1,7 @@
 (setq haskell-config-packages
       '(
         (haskell-config-flycheck :location local)
+        haskell-mode
         ))
 
 
@@ -12,7 +13,20 @@
     )
   )
 
+(defun haskell-config/post-init-haskell-mode ()
+  (setq haskell-process-args-ghci
+        '("-ferror-spans" "-fshow-loaded-modules"))
 
+  (setq haskell-process-args-cabal-repl
+        '("--ghc-options=-ferror-spans -fshow-loaded-modules"))
+
+  (setq haskell-process-args-stack-ghci
+        '("--ghci-options=-ferror-spans -fshow-loaded-modules"
+          "--no-build" "--no-load"))
+
+  (setq haskell-process-args-cabal-new-repl
+        '("--ghc-options=-ferror-spans -fshow-loaded-modules"))
+  )
 
 
 (defun haskell-config/flycheck-config-haskell ()
