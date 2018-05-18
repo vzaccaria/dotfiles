38942e459f2398cd331c20d47eb481a120e3502f
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2018-05-18 16:05:13 +0200

    minor: changes (github.sh)

--- zprezto/.zprezto-zack/github.sh
+++ zprezto/.zprezto-zack/github.sh
@@ -177,10 +177,19 @@ alias s='git status'
 
 alias gl="git log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
 
-alias git-show-last-commit-of="git whatchanged -n 1 -p"
 alias p='hub push --all'
 alias gr='git-chdir-into-root'
 
 alias lb='git branch'
 alias sb='git checkout '
-alias git-untrack='git update-index --assume-unchanged '
+
+
+alias vz-git-untrack='git update-index --assume-unchanged '
+alias vz-git-show-last-commit-of="git whatchanged -n 1 -p"
+alias vz-git-grep-all-commits='git log --source --all -S '
+alias vz-git-show-when-file-added='git log --diff-filter=A --'
+
+
+vz-git-show-commit() {
+    hub browse -- commit/$1
+}
