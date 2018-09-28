88bb0a500708aee5c7e3e43d2086fa8e77e1802e
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2018-09-28 18:27:30 +0200

    minor: changes (aliases.sh)

--- zprezto/.zprezto-zack/aliases.sh
+++ zprezto/.zprezto-zack/aliases.sh
@@ -53,3 +53,8 @@ alias mk='make -f makefile.mk'
 alias ag="ag --hidden --ignore '*.map'"
 
 alias gen-preview="convert -density 45 -depth 8 -quality 85 -delete 1--1"
+
+alias this-week-gcal="gcalcli calw 1 -w 15"
+alias next-week-gcal="gcalcli calw 1 'next week' -w 15"
+alias this-month-gcal="gcalcli calm -w 20"
+alias next-month-gcal="gcalcli calm 'next month' -w 20"
