5bc938ad746a2c3a023e6a36cba287f45e35b81a
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2018-11-04 15:40:44 +0100

    update: add muttrc mode (latex)

---
+++ spacemacs/.emacs.vz/private/config-muttrc/packages.el
@@ -0,0 +1,12 @@
+(setq config-muttrc-packages
+      '(
+        (muttrc-mode :location local)
+        ))
+
+(defun config-muttrc/init-muttrc-mode ()
+  (use-package muttrc-mode
+    :config
+    (progn)
+    )
+  )
+
