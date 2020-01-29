8ba662380ab8d9a358b63558fafb8b14dfbad2f2
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2020-01-29 09:49:42 +0100

    update: minor changes ()

---
+++ pm2/ecosystem.config.js
@@ -0,0 +1,11 @@
+[
+  {
+    "name": "serverino",
+    "script": "/usr/local/bin/serverino"
+  },
+  {
+    "name": "gotty",
+    "script": "/usr/local/bin/gotty",
+    "args": "-w -p 7565 top"
+  }
+]
