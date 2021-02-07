a698b31e650863c8d7191da33f192552362d60a0
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2021-02-07 14:17:24 +0100

    update: minor changes (aliases.sh)

--- zprezto/.zprezto-zack/Linux/aliases.sh
+++ zprezto/.zprezto-zack/Linux/aliases.sh
@@ -2,7 +2,6 @@
 alias open=xdg-open
 
 alias startanki='anki -b ~/Dropbox/Apps/Anki'
-alias vi='NVIM_LISTEN_ADDRESS=/tmp/nvimsocket nvim'
 alias pbcopy='xclip -selection clipboard -in'
 alias pbpaste='xclip -selection clipboard -out'
 
