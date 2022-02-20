2cb4a04de3a1565564d04f30b006d29c455f2cb0
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2022-02-20 18:56:18 +0100

    update: minor changes (devenv-most.Dockerfile, devenv.sh)

--- dockerfiles/envs/devenv/devenv-most.Dockerfile
+++ dockerfiles/envs/devenv/devenv-most.Dockerfile
@@ -25,6 +25,15 @@ ENV POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD true
 
 WORKDIR /root
 
-RUN apt-get install -y npm clang libedit-dev libxml2-dev silversearcher-ag bison flex mpich
+RUN apt-get install -y npm clang libedit-dev libxml2-dev silversearcher-ag bison flex mpich 
+RUN npm install -g diff-so-fancy
+RUN apt-get install -y clangd
+RUN npm install coc-clangd && nvim --headless +"CocInstall -sync coc-clangd" +qall
+RUN apt-get install -y cmake
+RUN apt-get install -y libreadline-dev
+RUN apt-get install -y libgsl-dev
+
+COPY ./overrides/most /root
+RUN apt-get install -y clang-format
 
 
--- dockerfiles/envs/devenv/devenv.sh
+++ dockerfiles/envs/devenv/devenv.sh
@@ -2,17 +2,18 @@
 
 set -e 
 
-CURD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+SRCD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+PWD="$(pwd)"
 command="$1"
 name="$2"
 lport="$3"
 
 if [ "${command}" = "build" ]; then 
-        docker build -f devenv-${name}.Dockerfile -t devenv-${name}-latest .
+        docker build -f ${SRCD}/devenv-${name}.Dockerfile -t devenv-${name}-latest ${SRCD}
 elif [ "${command}" = "gui-start" ]; then 
-        img=$(docker run -dt -v ${CURD}:/local -p ${lport}:5900 --privileged devenv-${name}-latest)
+        img=$(docker run -dt -v ${PWD}:/local -p ${lport}:5900 --privileged devenv-${name}-latest)
 elif [ "${command}" = "connect" ]; then 
-        docker run -ti --privileged -v ${CURD}:/local devenv-${name}-latest
+        docker run -ti --privileged -v ${PWD}:/local devenv-${name}-latest
 elif [ "${command}" = "gui-open-local" ]; then 
         /Applications/TigerVNC\ Viewer\ 1.12.0.app/Contents/MacOS/TigerVNC\ Viewer -SecurityTypes VncAuth ::${lport}
 fi
